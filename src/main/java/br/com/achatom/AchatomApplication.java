54a35d4ce643f96cb85f7122e317a2b5691e3d38
Kaique <kaiquejuvencio@hotmail.com>
2020-03-20 17:34:10 -0300

    Ajustado para Spring Boot

---
+++ src/main/java/br/com/achatom/AchatomApplication.java
@@ -0,0 +1,13 @@
+package br.com.achatom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AchatomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AchatomApplication.class, args);
+	}
+
+}
